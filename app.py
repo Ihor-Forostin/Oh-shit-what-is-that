c7a555725c2bbed97a6f1c40d306fb23435f7908
Ihor-Forostin <ihorforostin@gmail.com>
2022-03-07 01:03:21 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+name = input("Enter your name")
+input("Enter your date of birth")
+
+lst= ['Vitaly', 'Vitalik', 'Vitaliy', 'Vetal', 'Веталь', "Виталий", "Виталик", "Вєталь", "Віталій", "Віталя", "Віталька", "Віталік"]
+
+while True:
+    
+        name = input("Enter your name ")
+        input("Enter your date of birth ")
+        if name not in lst:
+            print('Sorry, an arror occured, please, try again. Probably you have entered the wrong name.')
+            
+        else:
+            print('oh shit i vse ravno naebal, otvet tut https://ihor-forostin.github.io/Error/')
+            break
+        
